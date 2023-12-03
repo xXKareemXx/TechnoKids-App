124a346c8d99845435d83159f892c30492c134cc
Kareem Akram <61750924+xXKareemXx@users.noreply.github.com>
2023-12-03 22:21:48 +0200

    Update footer.js

--- src/Components/html/footer.js
+++ src/Components/html/footer.js
@@ -15,17 +15,17 @@ function Footer () {
     <footer style={{marginBottom:"-50px"}}>
 
 <h2>Contact us:</h2>
-<p>Address: 123 Main St, Anytown USA</p>
+<p>Address: Horreia Road, Alexandria, Egypt </p>
 <p>E-mail:
   <a href="mailto:info@technokids.com">
-      info@technokids.com 
+      info@technokids.com
   </a>
 </p>
 <p>Phone: 1-905-631-9112</p>
 <h2>Follow us on social media:</h2>
-    <a href="https://www.facebook.com/profile.php?id=100002059013899" class="fa fa-facebook"></a>
-    <a href="https://twitter.com/TechnoKidsLA" class="fa fa-twitter"></a>
-    <a href="https://www.linkedin.com/in/abdelrahman-elzarka-software-engineer/" class="fa fa-linkedin"></a>
+    <a href="https://www.facebook.com/kareem.akram57" class="fa fa-facebook"></a>
+    <a href="https://twitter.com/Kareem0Akram" class="fa fa-twitter"></a>
+    <a href="https://www.linkedin.com/in/kareem-akram/" class="fa fa-linkedin"></a>
 <p>&copy; Technokids 2023</p>
 </footer>
 </>
